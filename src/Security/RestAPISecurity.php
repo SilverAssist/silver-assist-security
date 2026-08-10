7dfe7809f9ccb8412c5f8b81b9049e28eeeecf8a
Miguel Colmenares <me@miguelcolmenares.com>
2026-08-10 00:29:16 -0500

    style: Convert string concatenation to interpolation in rate limiting keys

--- src/Security/RestAPISecurity.php
+++ src/Security/RestAPISecurity.php
@@ -164,8 +164,8 @@ class RestAPISecurity {
 
 		// Use fixed-window rate limiting with explicit timestamp
 		$client_ip_hash = \sanitize_text_field( $client_ip );
-		$window_key     = 'silver_assist_rest_window_' . $client_ip_hash;
-		$count_key      = 'silver_assist_rest_limit_' . $client_ip_hash;
+		$window_key     = "silver_assist_rest_window_{$client_ip_hash}";
+		$count_key      = "silver_assist_rest_limit_{$client_ip_hash}";
 
 		// Get or create window start time
 		$window_start = (int) \get_transient( $window_key );
